f20663a5ab66009a1c793ba6c797a9cb92d25812
Caio Schnepper <caioschnepper@gmail.com>
2016-03-06 14:24:31 +0100

    Revert "Switch to common_mini_phone"
    Change-Id: I9d5d0f8a849c3f8a6e468428849376886ac95220

--- aicp.mk
+++ aicp.mk
@@ -21,9 +21,6 @@ PRODUCT_RELEASE_NAME := GT-I9100
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Inherit some common AICP stuff.
-$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9100/full_i9100.mk)
 
